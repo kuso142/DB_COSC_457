29969f7397fbb9df0acf7e6f7643f2f633357717
abbybuckland1 <abby_buckland@yahoo.com>
2026-05-09 17:39:37 -0400

    got rid of cuisine type

--- src/main/java/com/restaurantdeliverysystem/model/Vendor.java
+++ src/main/java/com/restaurantdeliverysystem/model/Vendor.java
@@ -9,23 +9,33 @@ public class Vendor {
     private String name;
     private String address;
     private String phoneNumber;
-    private String cuisineType;
 
     public Vendor(int vendorId, String name, String address,
-                  String phoneNumber, String cuisineType) {
-        this.vendorId    = vendorId;
-        this.name        = name;
-        this.address     = address;
+            String phoneNumber) {
+        this.vendorId = vendorId;
+        this.name = name;
+        this.address = address;
         this.phoneNumber = phoneNumber;
-        this.cuisineType = cuisineType;
     }
 
-    public int    getVendorId()    { return vendorId; }
-    public String getName()        { return name; }
-    public String getAddress()     { return address; }
-    public String getPhoneNumber() { return phoneNumber; }
-    public String getCuisineType() { return cuisineType; }
+    public int getVendorId() {
+        return vendorId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
 
     @Override
-    public String toString() { return name; }
+    public String toString() {
+        return name;
+    }
 }
